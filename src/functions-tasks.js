3d10425b21c91cfadec4c7074f7d2fd001596723
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-01-07 16:33:52 +0300

    feat: solve getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -52,8 +52,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => {
+    return func.length;
+  });
 }
 
 /**
